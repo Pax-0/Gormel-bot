f64b44df5fa10c7cf1f66bc93f9deb0e86ab5d05
unknown <abmamy82@gmail.com>
2020-04-18 12:36:08 +0400

    Updated link blacklist command, better handling of user error

--- commands/blackListedLink.js
+++ commands/blackListedLink.js
@@ -1,14 +1,18 @@
 const bot = require('../index');
-
+const URLRegex = new RegExp(/((http(s)?(:\/\/))+(www\.)?([\w\-./])*(\.[a-zA-Z]{2,3}\/?))[^\s\b\n|]*[^.,;:?!@^$ -]/);
 module.exports.generator = async (msg, args) => {
 	const settings = await bot.db.settings.findOne({});
 	if(!settings) return msg.channel.createMessage('Unable to locate bot settings..');
     
-	let blackListed = args;
-    
+	let blackListed = [];
+	args.forEach(element =>{
+		let link = element.match(URLRegex);
+		if(link) blackListed.push(link[0]);
+	});
+	if(!blackListed.length) return msg.channel.createMessage('Please use a valid website link.');
 	const sent = await msg.channel.createMessage('Updating links..');
 	await blackListLink(blackListed);
-	return sent.edit('Updated!');
+	return sent.edit('Updated blacklisted links!');
 };
 async function blackListLink(blackListed){
 	blackListed.forEach(async (element) => {
@@ -21,7 +25,7 @@ module.exports.options = {
 	description: 'Blacklist a website link.',
 	enabled: true,
 	argsRequired: true,
-	fullDescription:'Adds a website link to the blacklist, aka no longer usable in the server.',
+	fullDescription:'Adds a website link to the blacklist, aka no longer usable in the server, can add multiple links at a time, just seperate them with a space.',
 	usage:'Link',
 	requirements: {
 		custom: async (msg) => {
